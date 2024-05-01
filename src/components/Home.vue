bf44640a765d67cae9b738df5b74fe6f7d888ac1
jotacarvalh0 <josecarvalho11235@gmail.com>
2024-05-01 12:55:21 -0300

    Last news

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="homePage">
-    <div>
-        <h1>Teste</h1>
-      <div class="TituleNews">
-        <h1>ÚLTIMAS NOTICIAS</h1>
-        <H2></H2>
-      </div>
+  <main class="wrapper">
+    <section class="featured-box">
+      <div>
+        <section class="lastnewsTitule">
+          <h1 class="titleLast">ÚLTIMAS NOTICIAS</h1>
+          <H2 class="lineMain"></H2>
+        </section>
 
-      <div class="spaceNews">
-        <div class="new">
-          <div class="leftNews">
-            <img class="img-50" src="../assets/tristana_image.png">
-          </div>
-          <div class="rightnews">
-
+        <div class="spaceNews">
+          <div class="new">
+            <div class="leftNews">
+              <img class="img-50" src="../assets/tristana_image.png">
+            </div>
+            <div class="rightnews">
+              <img class="img-25 img25-1" src="../assets/valorant_image.png">
+              <img class="img-25" src="../assets/valorant_image.png">
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -28,16 +30,77 @@ export default {
 </script>
 
 <style scoped>
-.homePage {
+@import url('https://fonts.cdnfonts.com/css/gilroy-bold');
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.wrapper {
+  width: 100%; /* Tornar o wrapper 100% da largura do elemento pai */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.featured-box {
+  /* Adiciona margem superior e inferior para espaçamento */
+  margin-top: 20px;
+  margin-bottom: 20px;
   width: 80%;
+}
+
+.lastnewsTitule {
+  display: flex;
+  flex-direction: column;
+}
+
+.titleLast {
+  display: flex;
+  justify-content: left;
+  font-size: 30px;
+  font-family: 'Gilroy-Regular', sans-serif;
+  margin-bottom: 0px;
+}
+
+.lineMain {
+  border-bottom: 1px solid #ccc; /* Adiciona uma borda inferior cinza */
+  margin-bottom: 20px; /* Adiciona espaço abaixo da linha */
+}
+
+.spaceNews {
+
+}
+
+.new {
   display: flex;
   justify-content: center;
 }
-.spaceNews {
-  display: flex;
+
+.leftNews {
+
 }
-  .img-50 {
-    width: 100px;
-    height: 100px;
-  }
+
+.rightnews {
+  display: flex;
+  flex-direction: column;
+}
+
+.img-50 {
+  justify-content: left;
+  height: 400px;
+  margin-left: 0
+}
+
+.img-25 {
+  height: 190px;
+  margin-left: 20px;
+}
+
+.img25-1 {
+  margin-bottom: 18px;
+}
 </style>
