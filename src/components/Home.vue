99840ca17fe411a7b26b324e5b5de96081c71224
jotacarvalh0 <josecarvalho11235@gmail.com>
2024-05-01 14:06:27 -0300

    Main update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,10 +4,10 @@
       <div>
         <section class="lastnewsTitule">
           <h1 class="titleLast">ÚLTIMAS NOTICIAS</h1>
-          <H2 class="lineMain"></H2>
+          <H2 class="lineMain" id="line01"></H2>
         </section>
 
-        <div class="spaceNews">
+        <div>
           <div class="new">
             <div class="leftNews">
               <img class="img-50" src="../assets/tristana_image.png">
@@ -16,6 +16,25 @@
               <img class="img-25 img25-1" src="../assets/valorant_image.png">
               <img class="img-25" src="../assets/valorant_image.png">
             </div>
+          </div>
+        </div>
+      </div>
+      <section class="lastnewsTitule2">
+        <H2 class="lineMain"></H2>
+        <h1 class="titleLast">CRONOGRAMA</h1>
+      </section>
+      <div class="cronograma-new">
+        <div class="newCronograma">
+          <div class="leftCro">
+            <h1 class="subTitleLast"> TERÇA 23/04</h1>
+            <div>
+              <img class="ImageNew" src="../assets/cs2_image.png">
+            </div>
+          </div>
+          <div class="textnew">
+            <h1 class="namePost">COUNTER STRIKER 2</h1>
+            <h1 class="subPost">ERROR 404</h1>
+            <p class="subtitle">Erros da Valve com CS2 já têm consequências e algo precisa ser feito.</p>
           </div>
         </div>
       </div>
@@ -56,6 +75,7 @@ body {
 .lastnewsTitule {
   display: flex;
   flex-direction: column;
+  margin-top: 20px;
 }
 
 .titleLast {
@@ -68,20 +88,16 @@ body {
 
 .lineMain {
   border-bottom: 1px solid #ccc; /* Adiciona uma borda inferior cinza */
-  margin-bottom: 20px; /* Adiciona espaço abaixo da linha */
+  margin-bottom: 0px; /* Adiciona espaço abaixo da linha */
 }
 
-.spaceNews {
-
+#line01 {
+  margin-bottom: 20px;
 }
 
 .new {
   display: flex;
   justify-content: center;
-}
-
-.leftNews {
-
 }
 
 .rightnews {
@@ -102,5 +118,53 @@ body {
 
 .img25-1 {
   margin-bottom: 18px;
+}
+
+.lastnewsTitule2 {
+  display: flex;
+  justify-content: left;
+}
+.cronograma-new .new {
+  justify-content: flex-start; /* Alinha o .new à esquerda */
+}
+
+.newCronograma {
+  display: flex;
+}
+
+.leftCro {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.subTitleLast {
+  font-size: 18px;
+  font-family: 'Gilroy-Bold', sans-serif;
+}
+
+.ImageNew {
+    height: 220px;
+    width: 400px;
+}
+
+.textnew {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+}
+
+.namePost {
+  margin: 0;
+}
+
+.subPost {
+  margin: 0;
+}
+
+.subtitle {
+  margin-bottom: 0;
+  margin-top: 2px;
 }
 </style>
